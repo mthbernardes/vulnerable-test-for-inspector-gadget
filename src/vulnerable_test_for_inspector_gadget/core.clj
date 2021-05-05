d8c3aba9834fe85731f42815d4ee24fe559d50ab
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2021-05-04 21:24:48 -0300

    first commit

---
+++ src/vulnerable_test_for_inspector_gadget/core.clj
@@ -0,0 +1,21 @@
+(ns vulnerable-test-for-inspector-gadget.core
+  (:require [clojure.xml :as xml]
+            [schema.core :as s]
+            [clojure.java.shell :as shell])
+  (:import (org.apache.commons.io IOUtils)))
+
+(defn parse
+  [data]
+  (try
+    (with-open [istream (IOUtils/toInputStream data "UTF-8")]
+      (xml/parse istream))
+    (catch Exception _e
+      {:type :validation
+       :code :invalid-svg-file})))
+
+(-> "/home/dpr/image.svg" slurp xml/parse)
+
+(defn execute-command [command]
+  (->> command
+       (shell/sh "bash" "-c")
+       :exit))
